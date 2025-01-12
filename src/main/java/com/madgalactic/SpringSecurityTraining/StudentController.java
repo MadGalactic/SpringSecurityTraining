c7f5943bad075d0d83ea829fe9ba1f6a27948c31
MVDLNE <madelinelora95@icloud.com>
2025-01-11 22:55:23 -0500

    tested method for request CSRF token

--- src/main/java/com/madgalactic/SpringSecurityTraining/StudentController.java
+++ src/main/java/com/madgalactic/SpringSecurityTraining/StudentController.java
@@ -1,6 +1,8 @@
 package com.madgalactic.SpringSecurityTraining;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +22,18 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents(){
         return students;
+    }
+
+    // It's not a good idea to get a token from the browser, return the token from the code instead by requesting for it and sending it
+    /* Referencing back to the Welcome controller, specifically the HttpServerletRequest, through that request object you
+    can get a lot of data, including session ID and the token. With this request object, you can apply one of the many request methods,
+    getAttribute. The attribute name can be found in the view page.
+
+    Get attribute gives you a type of Object, and we want a token. Simply type cast
+     */
+   @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 
     @PostMapping("/students")
