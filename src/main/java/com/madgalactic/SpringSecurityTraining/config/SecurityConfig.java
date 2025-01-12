5bc2fd004c1697966df0c272868ce789d2e03cae
MVDLNE <madelinelora95@icloud.com>
2025-01-12 16:52:58 -0500

    exploring ways of disabling csrf

--- src/main/java/com/madgalactic/SpringSecurityTraining/config/SecurityConfig.java
+++ src/main/java/com/madgalactic/SpringSecurityTraining/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,14 +24,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        //disable csrf
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated()); /* require authentication*/
-//        http.formLogin(Customizer.withDefaults()); /* form login implementation*/
-        http.httpBasic(Customizer.withDefaults()); /* for rest api access */
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        /* HttpSecurity is an object with several methods, one being csrf.  */
 
-        return http.build(); /* the build method allows you to return the security filter chain */
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .build();
+
+
+        //disable csrf
+//        http.csrf(customizer -> customizer.disable()); /* In this step, you are passing the customizer in the method */
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated()); /* lambda expression says that for every request, requests need to be authenticated. with this, no one should be able to access the page without authentication*/
+//        http.formLogin(Customizer.withDefaults()); /* form login implementation*/
+//        http.httpBasic(Customizer.withDefaults()); /* for rest api access */
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); /* if you make your session stateless, you don't need to worry about the session ID.*/
+
+
+        // another way to disable csrf
+//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+//                customizer.disable();
+//            }
+//        };
+
+//        http.csrf(custCsrf);
+
+//        return http.build(); /* the build method allows you to return the security filter chain */
     }
 
 
